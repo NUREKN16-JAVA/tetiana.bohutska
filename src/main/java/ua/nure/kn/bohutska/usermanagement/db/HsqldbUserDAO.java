83f4865829d87d8a3cf83674753c2ee0e6264c00
Tatiana <tetiana.bohutska@nure.ua>
2018-11-28 13:21:39 +0200

    5 step

--- src/main/java/ua/nure/kn/bohutska/usermanagement/db/HsqldbUserDAO.java
+++ src/main/java/ua/nure/kn/bohutska/usermanagement/db/HsqldbUserDAO.java
@@ -2,9 +2,16 @@ package ua.nure.kn.bohutska.usermanagement.db;
 
 import ua.nure.kn.bohutska.usermanagement.User;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
+
 public class HsqldbUserDAO implements UserDAO {
+
+    private static final String INSERT_QUERY = "INSERT INTO USERS (FIRSTNAME, LASTNAME, DATEOFBIRTH) VALUES(?, ?, ?)";
 
     ConnectionFactory connectionFactory;
     public  HsqldbUserDAO(ConnectionFactory connectionFactory){
@@ -12,7 +19,21 @@ public class HsqldbUserDAO implements UserDAO {
     }
     @Override
     public User create(User user) throws DatabaseException {
+    	
+        try{Connection connection = connectionFactory.createConnection();
+        PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setDate(3, new Date(user.getDateOfBirth().getTime()));
+
+        int n = statement.executeUpdate();
+        if (n != 1) throw new DatabaseException("Numbers of Inserted rows: " + n);
         return null;
+        }
+        catch(DatabaseException e) {throw e;}
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
     }
 
     @Override
